eefd08bf4041730a2037293b9f977863484a4786
Jerumeh lawrence <jerumehlawrence@gmail.com>
2023-01-02 23:34:33 +0100

    updated

--- simple_shell/line_exec.c
+++ simple_shell/line_exec.c
@@ -70,40 +70,48 @@ void _realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
     char *new;
     char *old;
-
     unsigned int i;
-    if (ptr == NULL)
-        return (malloc(new_size));
 
-    if (new_size == old_size)
-        return ptr;
+    if (ptr == NULL)
+    {
+        return malloc(sizeof(new_size));
+    }
+
     if (new_size == 0 || !ptr)
     {
         free(ptr);
         return NULL;
     }
 
-    new = malloc(new_size);
+    new = malloc(sizeof(new_size));
     old = ptr;
+
     if (new == NULL)
         return NULL;
 
+    if (new_size == old_size)
+        return ptr;
+
     if (new_size > old_size)
     {
-        for (i = 0; i < old_size; i++)
+        for (i = 0; i < new_size; i++)
         {
             new[i] = old[i];
         }
         free(ptr);
+
         for (i = old_size; i < new_size; i++)
         {
-            new[i] = '\0';
+            new[i] = "\0";
         }
+
         if (new_size < old_size)
         {
             for (i = 0; i < new_size; i++)
+            {
                 new[i] = old[i];
-            free(ptr);
+                free(ptr);
+            }
         }
     }
     return (new);
